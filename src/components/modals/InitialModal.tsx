f58eff46944ea86d38d50a20c74ece7536a40de2
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-03 14:30:27 +0530

    image upload done

--- src/components/modals/InitialModal.tsx
+++ src/components/modals/InitialModal.tsx
@@ -22,6 +22,7 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import FileUpload from "../FileUpload";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Server name is required" }),
@@ -68,7 +69,21 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8">
             <div className=" space-y-8 px-6">
               <div className=" flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
               <FormField
