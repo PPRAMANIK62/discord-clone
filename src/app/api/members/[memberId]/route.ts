2c42f82469bd3473d28a1a85336c9853c90e2f58
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-04 15:12:13 +0530

    member role change done

---
+++ src/app/api/members/[memberId]/route.ts
@@ -0,0 +1,61 @@
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { memberId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+    const { role } = await req.json();
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    if (!serverId)
+      return new NextResponse("Server ID Missing", { status: 400 });
+
+    if (!params.memberId)
+      return new NextResponse("Member ID Missing", { status: 400 });
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        Members: {
+          update: {
+            where: {
+              id: params.memberId,
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        Members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.error("[MEMBERS_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- src/components/providers/ModalProvider.tsx
+++ src/components/providers/ModalProvider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import CreateServerModal from "../modals/CreateServerModal";
 import EditServerModal from "../modals/EditServerModal";
 import InviteModal from "../modals/InviteModal";
+import MembersModal from "../modals/MembersModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState<boolean>(false);
@@ -18,6 +19,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 };
