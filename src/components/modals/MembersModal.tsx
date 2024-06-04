ff735a95fcce5f3a848388b328ae25e6d57f1efa
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-04 15:22:17 +0530

    kicking a member done

--- src/components/modals/MembersModal.tsx
+++ src/components/modals/MembersModal.tsx
@@ -52,6 +52,28 @@ const MembersModal = () => {
   const isModalOpen = isOpen && type === "members";
   const { server } = data as { server: ServerWithMembersWithProfiles };
 
+  const onKick = async (memberId: string) => {
+    try {
+      setLoadingId(memberId);
+
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      const res = await axios.delete(url);
+
+      router.refresh();
+      onOpen("members", { server: res.data });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoadingId("");
+    }
+  };
+
   const onRoleChange = async (memberId: string, role: MemberRole) => {
     try {
       setLoadingId(memberId);
@@ -143,7 +165,7 @@ const MembersModal = () => {
 
                         <DropdownMenuSeparator />
 
-                        <DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => onKick(member.id)}>
                           <Gavel className=" h-4 w-4 mr-2" />
                           Kick
                         </DropdownMenuItem>
