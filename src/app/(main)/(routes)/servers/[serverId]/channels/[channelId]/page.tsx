fb6cbc1bc6b4fb231541c103f2fdef218d228cb0
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-12 14:18:54 +0530

    always redirecting to general when clicking on any server

---
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>channel page</div>;
+};
+
+export default page;
--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>page</div>;
+interface PageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const page = async ({ params }: PageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      Members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      Channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.Channels[0];
+
+  if (initialChannel?.name !== "general") return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 };
 
 export default page;
