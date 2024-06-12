fb5285335516447ca06f5ff7bceb6a54a2858b6d
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-12 15:30:15 +0530

    user conversation initiation done

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -1,19 +1,25 @@
-import { Hash, Menu } from "lucide-react";
+import { Hash } from "lucide-react";
 import MobileTogggle from "../MobileTogggle";
+import UserAvatar from "../UserAvatar";
 
 interface ChatHeaderProps {
   serverId: string;
   name: string;
   type: "channel" | "conversation";
+  imageUrl?: string;
 }
 
-const ChatHeader = ({ name, serverId, type }: ChatHeaderProps) => {
+const ChatHeader = ({ name, serverId, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className=" text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileTogggle serverId={serverId} />
 
       {type === "channel" && (
         <Hash className=" w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className=" h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
 
       <p className=" font-semibold text-md text-black dark:text-white">
