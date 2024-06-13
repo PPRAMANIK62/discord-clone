4b0adb3d21d53c49851c71526646970b1ec29ca0
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-13 13:05:24 +0530

    chat input done

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/ChatHeader";
+import ChatInput from "@/components/chat/ChatInput";
 import { currentProfile } from "@/lib/currentProfile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs/server";
@@ -37,6 +38,18 @@ const page = async ({ params }: PageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+
+      <div className=" flex-1">Future Messages</div>
+
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
